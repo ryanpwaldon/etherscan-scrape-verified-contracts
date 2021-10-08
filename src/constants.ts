328faaa770aa6d02574ad4a1abffd11023fcf76d
Ryan Waldon <ryanpwaldon@gmail.com>
2021-10-09 05:17:57 +1100

    feat: updates

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY
-export const AIRTABLE_BASE_ID = 'appWmsJnlpwW4WjaG'
 export const AIRTABLE_TABLE_NAME = 'contracts'
+export const AIRTABLE_BASE_ID = 'appWmsJnlpwW4WjaG'
+export const AIRTABLE_API_KEY = <string>process.env.AIRTABLE_API_KEY
+export const SLACK_WEBHOOK_URL = <string>process.env.SLACK_WEBHOOK_URL
--- src/modules/slack.ts
+++ src/modules/slack.ts
@@ -1,6 +1,7 @@
 import { IncomingWebhook } from '@slack/webhook'
+import { SLACK_WEBHOOK_URL } from '../constants'
 
-const client = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL)
+const client = new IncomingWebhook(SLACK_WEBHOOK_URL)
 
 export const sendText = (text: string) => {
   client.send({ text })
